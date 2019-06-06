5363aa432e85d0df52eb783833809d064e1dc29c
sayaliupasani1 <sayaliupasani@gmail.com>
2019-06-06 12:21:30 -0700

    Initial commit for URL status check app

---
+++ status_project/status_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def webpage():
+    return render_template('webpage.html')
+
+@app.route('/', methods=['POST'])
+def web_url():
+    url = request.form['url']
+    processed_url = url.lower()
+    urlcheck = requests.get(processed_url)
+    #print(urlcheck.status_code)
+    code = urlcheck.status_code
+    return render_template('webpage.html', url = processed_url,  status=code)
+
+if __name__=='__main__':
+    app.run()
+#    statuscheck(processed_url)
+
+#def statuscheck(url_check):
+#    request = requests.get(url_check)
+#    print(request.status_code)
+
