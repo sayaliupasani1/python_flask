a02ae3b19061091027ecd95087a74324c9eb8b23
sayaliupasani1 <sayaliupasani@gmail.com>
2019-06-06 15:06:59 -0700

    Added some comments to status_app.py

--- status_project/status_app.py
+++ status_project/status_app.py
@@ -3,24 +3,26 @@ import requests
 
 app = Flask(__name__)
 
+# If the user comes on / path, it gets redirected to webpage.html saved under /templates directory.
+
 @app.route('/')
 def webpage():
     return render_template('webpage.html')
+
+# render_template will render and display the specified html page.
 
 @app.route('/', methods=['POST'])
 def web_url():
     url = request.form['url']
     processed_url = url.lower()
     urlcheck = requests.get(processed_url)
+    # requests.get will perform an GET request to specified URL and fetch the response code.
     #print(urlcheck.status_code)
     code = urlcheck.status_code
-    return render_template('webpage.html', url = processed_url,  status=code)
+    # status_code displays the http response code from requests.get method
+    return render_template('webpage.html', url = processed_url,  status=code)#render_template is passed with some values that are written as variables in html file
+
 
 if __name__=='__main__':
-    app.run()
-#    statuscheck(processed_url)
-
-#def statuscheck(url_check):
-#    request = requests.get(url_check)
-#    print(request.status_code)
+    app.run() # runs the flask app and starts listening on localhost:5000
 
