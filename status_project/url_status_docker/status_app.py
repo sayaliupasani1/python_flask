06c9a90eff37163e98eb7847bf7afd81df0b6040
sayaliupasani1 <sayaliupasani@gmail.com>
2019-06-06 15:47:31 -0700

    Added code to catch incorrect url and connection exceptions

--- status_project/url_status_docker/status_app.py
+++ status_project/url_status_docker/status_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import requests
+from requests.exceptions import ConnectionError, MissingSchema
 
 app = Flask(__name__)
 
@@ -11,10 +12,14 @@ def webpage():
 def web_url():
     url = request.form['url']
     processed_url = url.lower()
-    urlcheck = requests.get(processed_url)
-    #print(urlcheck.status_code)
-    code = urlcheck.status_code
-    return render_template('webpage.html', url = processed_url,  status=code)
+    try:
+        urlcheck = requests.get(processed_url)
+        code = urlcheck.status_code
+        return render_template('webpage.html', url = processed_url,  status=code)
+    except ConnectionError:
+        return render_template('webpage.html', url = processed_url, status = 'The website does not exist')
+    except MissingSchema:
+        return render_template('webpage.html', url = processed_url, status = 'The URL format is incorrect. Please try again!')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
