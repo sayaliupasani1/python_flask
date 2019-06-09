937e9e86c14f750940e35ee07a47af9a98e4ea02
sayaliupasani1 <sayaliupasani@gmail.com>
2019-06-09 16:13:41 -0700

    Added a code for displaying accessed URL cache and rendering it from dictionary with an expiration of 10 minutes

--- status_project/url_status_docker/status_app.py
+++ status_project/url_status_docker/status_app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template
 import requests
 from requests.exceptions import ConnectionError, MissingSchema
+import time
 
 app = Flask(__name__)
-
+url_dict = {}
 @app.route('/') #If the user comes on /path, it gets redirected to webpage.htm saved under templates directory
 
 def webpage():
@@ -14,13 +15,28 @@ def web_url():
     url = request.form['url']
     processed_url = url.lower()
     try:
-        urlcheck = requests.get(processed_url)
-        code = urlcheck.status_code
-        if code == 200:
-            return render_template('webpage.html', url = processed_url,  status=code, availability = 'The website you specified is availble!')
+        if processed_url in url_dict:
+            if url_dict[processed_url][0]- int(time.time()) > 600:
+                urlcheck = requests.get(processed_url)
+                code = urlcheck.status_code
+                url_dict[processed_url] = [int(time.time()), code]
+                if code == 200:
+                    return render_template('webpage.html', url = processed_url,  status=code, availability = 'The website you specified is availble!', cache = url_dict.items())
+                else:
+                    return render_template('webpage.html', url = processed_url, status=code,
+                                   availability = 'The website you specified returned {}!'.format(code), cache = url_dict.keys())
+            else:
+                return render_template('webpage.html', url = processed_url, status=url_dict[processed_url][1], availability = 'As in dict, yet to add', cache = url_dict.keys())
         else:
-            return render_template('webpage.html', url = processed_url, status=code,
-                                   availability = 'The website you specified returned {}!'.format(code))
+            urlcheck = requests.get(processed_url)
+            code = urlcheck.status_code
+            url_dict[url] = [int(time.time()), code]
+            if code == 200:
+                return render_template('webpage.html', url = processed_url,  status=code, availability = 'The website you specified is availble!', cache = url_dict.items())
+            else:
+                return render_template('webpage.html', url = processed_url, status=code,
+                                       availability = 'The website you specified returned {}!'.format(code), cache = url_dict.keys())
+        
     except ConnectionError:
         return render_template('webpage.html', url = processed_url, status = 'The website does not exist')
     except MissingSchema:
